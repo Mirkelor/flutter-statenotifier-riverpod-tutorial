92e25ca2f66d550e736a37b8794aab64886212d5
Hasan Efe <hefe@hurriyetemlak.com>
2020-12-12 17:29:35 +0300

    riverpod added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod_statenotifier_tutorial/presentation/weather_search_page.dart';
+import 'package:flutter_riverpod/all.dart';
+
+import 'presentation/weather_search_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: WeatherSearchPage(),
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Material App',
+        home: WeatherSearchPage(),
+      ),
     );
   }
 }
